9357a609504f5d7800c5e579e22bc8456c6e8b81
m-samhitha <228W1A12B0@vrsec.ac.in>
2024-02-26 10:08:33 +0530

    data frames

---
+++ df1.R
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+Name <- c("Alex", "Lilly", "Mark", "Oliver", "Martha", "Lucas", "Caroline")
+Age <- c(25, 31, 23, 52, 76, 49, 26)
+Height <- c(177, 163, 190, 179, 163, 183, 164)
+Weight <- c(57, 69, 83, 75, 70, 83, 53)
+Sex <- as.factor(c("F", "F", "M", "M", "F", "M", "F"))
+df <- data.frame (row.names = Name, Age, Height, Weight, Sex)
+levels(df$Sex)
+levels(df$Sex) <- c("M","F")
+df
+=======
+  Name <- c("Alex", "Lilly", "Mark", "Oliver", "Martha", "Lucas", "Caroline")
+Age <- c(25, 31, 23, 52, 76, 49, 26)
+Height <- c(177, 163, 190, 179, 163, 183, 164)
+Weight <- c(57, 69, 83, 75, 70, 83, 53)
+Sex <- as.factor(c("F", "F", "M", "M", "F", "M", "F"))
+df <- data.frame (row.names = Name, Age, Height, Weight, Sex)
+levels(df$Sex)
+levels(df$Sex) <- c("M","F")
+df
+>>>>>>> bf10c649652cd83aec2e0c9316609286d872ebcc
+
---
+++ df2.R
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+Working <- c("Yes","No","No","Yes","Yes","No","Yes")
+
+df2 <- data.frame(row.names = Name,Working) #Name has been already defined in exercise 1.
+
+df2
+
+class(state.center)
+=======
+  Working <- c("Yes","No","No","Yes","Yes","No","Yes")
+
+df2 <- data.frame(row.names = Name,Working) #Name has been already defined in exercise 1.
+
+df2
+
+class(state.center)
+>>>>>>> bf10c649652cd83aec2e0c9316609286d872ebcc
+
